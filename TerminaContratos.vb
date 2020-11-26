9499b5c20ffa89d789e40997171fa0d7b2520648
ecacerest <ecacerest@finagil.com.mx>
2020-11-26 10:44:32 -0600

    Cambio TerminaContratos

--- TerminaContratos.vb
+++ TerminaContratos.vb
@@ -1,32 +1,31 @@
 ﻿Module TerminaContratos
-
+    Dim CorreosFases As New ProduccionDSTableAdapters.GEN_CorreosFasesTableAdapter
+    Dim TCorreosFases As New ProduccionDS.GEN_CorreosFasesDataTable
     Public Sub Termina_Contratos()
         Dim TA As New ProduccionDSTableAdapters.AnexosTerminadosTableAdapter
         Dim T As New ProduccionDS.AnexosTerminadosDataTable
-        Dim ta2 As New WEB_FinagilDSTableAdapters.CorreosTableAdapter
-        Dim t2 As New WEB_FinagilDS.CorreosDataTable
 
         TA.Fill(T, Today.AddDays(1).ToString("yyyyMMdd"))
-        ta2.Fill(t2, "TERMINACONTRATO")
+        CorreosFases.Fill(TCorreosFases, "TERMINACONTRATO")
         For Each R As ProduccionDS.AnexosTerminadosRow In T.Rows
             TA.DesbloqueaAnexo(R.Anexo)
             TA.TerminaContrato(R.Anexo)
             TA.BloqueaAnexo(R.Anexo)
             If R.SaldoFac < 10 Then
-                For Each rr As WEB_FinagilDS.CorreosRow In t2.Rows
+                For Each rr As ProduccionDS.GEN_CorreosFasesRow In TCorreosFases.Rows
                     Utilerias.EnviacORREO(rr.Correo, R.AnexoCon, "Terminación de Contrato: " & R.AnexoCon, "Notificaciones@finagil.com.mx")
                 Next
             End If
         Next
         'Cancelados por Adelanto a capital
         TA.FillSinSaldoInsoluto(T, Today.AddDays(1).ToString("yyyyMMdd"))
-        ta2.Fill(t2, "TERMINACONTRATO")
+        CorreosFases.Fill(TCorreosFases, "TERMINACONTRATO")
         For Each R As ProduccionDS.AnexosTerminadosRow In T.Rows
             TA.DesbloqueaAnexo(R.Anexo)
             TA.CancelaContrato(R.Anexo)
             TA.BloqueaAnexo(R.Anexo)
             If R.SaldoFac < 10 Then
-                For Each rr As WEB_FinagilDS.CorreosRow In t2.Rows
+                For Each rr As ProduccionDS.GEN_CorreosFasesRow In TCorreosFases.Rows
                     Utilerias.EnviacORREO(rr.Correo, R.AnexoCon, "Cancelación de Contrato: " & R.AnexoCon, "Notificaciones@finagil.com.mx")
                 Next
             End If
@@ -37,10 +36,7 @@
     Public Sub Terminados_Con_Saldo(fec As Date)
         Dim TA As New ProduccionDSTableAdapters.Vw_TerminadosConSaldoTableAdapter
         Dim T As New ProduccionDS.Vw_TerminadosConSaldoDataTable
-
-        Dim ta2 As New WEB_FinagilDSTableAdapters.CorreosTableAdapter
-        Dim t2 As New WEB_FinagilDS.CorreosDataTable
-        ta2.Fill(t2, "TERMINACONTRATO")
+        CorreosFases.Fill(TCorreosFases, "TERMINACONTRATO")
 
         TA.Fill(T)
         For Each R As ProduccionDS.Vw_TerminadosConSaldoRow In T.Rows
@@ -51,7 +47,7 @@
         TA.TermiandosConSaldo_liquidados(T)
         For Each R As ProduccionDS.Vw_TerminadosConSaldoRow In T.Rows
             TA.TerminaAnexo(R.Anexo)
-            For Each rr As WEB_FinagilDS.CorreosRow In t2.Rows
+            For Each rr As ProduccionDS.GEN_CorreosFasesRow In TCorreosFases.Rows
                 Utilerias.EnviacORREO(rr.Correo, R.Anexo, "Terminación de Contrato (Saldo Pagado): " & R.Anexo, "Notificaciones@finagil.com.mx")
             Next
         Next
